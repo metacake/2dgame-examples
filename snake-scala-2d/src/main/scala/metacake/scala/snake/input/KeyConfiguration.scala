f122ff4e7ae8e9e1bb64a653e1ddf7d471979209
Ryan Plessner <plessner.ryan@gmail.com>
2014-05-10 15:58:46 -0400

    changed over to the click recognizer

--- snake-scala-2d/src/main/scala/metacake/scala/snake/input/KeyConfiguration.scala
+++ snake-scala-2d/src/main/scala/metacake/scala/snake/input/KeyConfiguration.scala
@@ -1,7 +1,7 @@
 package metacake.scala.snake.input
 
 import io.metacake.core.process.ActionRecognizerName
-import io.metacake.s2d.process.recognizers.keyboard.{KeyReleaseActionRecognizer, KeyHoldActionRecognizer, KeyActionRecognizer}
+import io.metacake.s2d.process.recognizers.keyboard.{KeyClickActionRecognizer, KeyReleaseActionRecognizer, KeyHoldActionRecognizer, KeyActionRecognizer}
 import io.metacake.core.input.ActionTrigger
 import java.awt.event.KeyEvent
 import io.metacake.s2d.input.keyboard.KeyboardActionTrigger
@@ -9,22 +9,22 @@ import io.metacake.s2d.input.keyboard.KeyboardActionTrigger
 object KeyConfiguration {
 
   val W: ActionRecognizerName = new ActionRecognizerName()
-  val wRecognizer: KeyActionRecognizer = new KeyHoldActionRecognizer(W)
+  val wRecognizer: KeyActionRecognizer = new KeyClickActionRecognizer(W)
   val wTrigger: ActionTrigger[KeyEvent] = new KeyboardActionTrigger(KeyEvent.VK_W).bindRecognizer(wRecognizer)
 
   val S: ActionRecognizerName = new ActionRecognizerName()
-  val sRecognizer: KeyActionRecognizer = new KeyHoldActionRecognizer(S)
+  val sRecognizer: KeyActionRecognizer = new KeyClickActionRecognizer(S)
   val sTrigger: ActionTrigger[KeyEvent] = new KeyboardActionTrigger(KeyEvent.VK_S).bindRecognizer(sRecognizer)
 
   val A: ActionRecognizerName = new ActionRecognizerName()
-  val aRecognizer: KeyActionRecognizer = new KeyHoldActionRecognizer(A)
+  val aRecognizer: KeyActionRecognizer = new KeyClickActionRecognizer(A)
   val aTrigger: ActionTrigger[KeyEvent] = new KeyboardActionTrigger(KeyEvent.VK_A).bindRecognizer(aRecognizer)
 
   val D: ActionRecognizerName = new ActionRecognizerName()
-  val dRecognizer: KeyActionRecognizer = new KeyHoldActionRecognizer(D)
+  val dRecognizer: KeyActionRecognizer = new KeyClickActionRecognizer(D)
   val dTrigger: ActionTrigger[KeyEvent] = new KeyboardActionTrigger(KeyEvent.VK_D).bindRecognizer(dRecognizer)
 
   val ESCAPE: ActionRecognizerName = new ActionRecognizerName()
-  val escRecognizer: KeyActionRecognizer = new KeyReleaseActionRecognizer(ESCAPE)
+  val escRecognizer: KeyActionRecognizer = new KeyClickActionRecognizer(ESCAPE)
   val escTrigger: ActionTrigger[KeyEvent] = new KeyboardActionTrigger(KeyEvent.VK_ESCAPE).bindRecognizer(escRecognizer)
 }
